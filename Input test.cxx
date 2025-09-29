35bf15ded8564e4310e91a39469473b08f11d128
kycian <kycian098@gmail.com>
2025-09-29 16:20:49 +0800

    Add files via upload

---
+++ Input test.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int number;
+   cout <<"your favorite number: ";
+   cin >>number;
+   
+   cout <<"your favorite number is: " <<number;
+   
+   
+   return 0;
+}
